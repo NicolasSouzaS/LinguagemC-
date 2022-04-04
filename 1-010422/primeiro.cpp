92a4063abdf475587dc5d7296abc783d7bcf7eaa
NicolasSouzaS <nicolas.souzapb@gmail.com>
2022-04-04 08:52:44 -0300

    Estudo C++

---
+++ 1-010422/primeiro.cpp
@@ -0,0 +1,21 @@
+/*
+Programa para exibir uma mensagem. O código foi escrito em
+C++
+*/
+
+/*
+Importação da biblioteca de entrada e saída de dados
+*/
+
+#include <iostream>
+
+int main()
+{
+
+    //Saída padrão de dados
+    //"cout" (console out -> Saída na tela de console(terminal) )
+    std::cout << "Bem vindo ao C++\n";
+
+    return 0;
+    
+}
